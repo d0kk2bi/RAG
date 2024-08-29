0775afea4416e231ec6111dd6fc2efd16cdffd41
d0kk2bi <kyonggi@kakao.com>
2024-08-29 23:02:56 +0900

    alter: filename

--- langchain/02_splitting/02_RecursiveCharacterTextSplitter.py
+++ langchain/02_splitting/02_RecursiveCharacterTextSplitter.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import TextLoader
 
 # langchain_text_splitters package separated with langchain package
 # langchain.text_splitter -> langchain_text_splitters (< langchain v0.1.11)
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 path = "test.txt"
 
@@ -17,7 +17,7 @@ print("base content:", data[0].page_content)
 # https://api.python.langchain.com/en/latest/text_splitters/character/langchain_text_splitters.character.RecursiveCharacterTextSplitter.html#recursivecharactertextsplitter
 # https://python.langchain.com/v0.1/docs/modules/data_connection/document_transformers/recursive_text_splitter/
 # Splitting text by recursively look at characters
-text_splitter = CharacterTextSplitter(
+text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=50,  # Maximum size of chunks to return
     chunk_overlap=10,  # Overlap in characters between chunks
     length_function=len,  # Function that measures the length of given chunks
