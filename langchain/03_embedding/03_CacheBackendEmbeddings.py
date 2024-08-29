d04ad5ed6c4a8f72ea7c3271d6ef18b1df831332
hannbyul <mail@hanbyul.me>
2024-08-30 02:05:40 +0900

    fix: CacheBackendEmbeddings

--- langchain/03_embedding/03_CacheBackendEmbeddings.py
+++ langchain/03_embedding/03_CacheBackendEmbeddings.py
@@ -1,5 +1,6 @@
+from langchain.embeddings import CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
-from langchain_openai import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 # https://api.python.langchain.com/en/latest/langchain/storage/langchain.storage.file_system.LocalFileStore.html#localfilestore
 # Base Storage class for local file system storage
@@ -10,16 +11,15 @@ texts = [
     "'Bye World' is my favorite sentence!",
 ]
 
-# https://api.python.langchain.com/en/latest/openai/embeddings/langchain_openai.embeddings.base.OpenAIEmbeddings.html#openaiembeddings
 embed = OpenAIEmbeddings(model="text-embedding-3-large")
 
-
-# cache_embed = CacheBackedEmbeddings
+# https://api.python.langchain.com/en/latest/langchain/embeddings/langchain.embeddings.cache.CacheBackedEmbeddings.html#cachebackedembeddings
+cached_embed = CacheBackedEmbeddings.from_bytes_store(embed, cache_dir)
 
 # Embed Single Text
-vector = embed.embed_query(texts[0])
+vector = cached_embed.embed_query(texts[0])
 print("vector:", vector[:3])
 
 # Embed Multiple Texts
-vectors = embed.embed_documents(texts)
+vectors = cached_embed.embed_documents(texts)
 print("vector:", vectors[1][:3])
